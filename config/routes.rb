62301bf57385fcf52b313030a7c88f7514a62051
Jensen <jensenkhem@gmail.com>
2020-09-06 14:09:10 -0600

    Added testing and title for home - incomplete for help, about page

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   # Defines the correspondance between web pages and URL's
   get 'static_pages/home'
   get 'static_pages/help'
+  get 'static_pages/about'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root 'application#hello'
 end
