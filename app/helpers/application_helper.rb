b7ecd11088110e8b910f3a44b6da810a806a72bc
Jensen <jensenkhem@gmail.com>
2020-09-09 16:22:00 -0600

    Finished chapter 3 and 4

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,11 @@
 module ApplicationHelper
+    # Returns the full title on a per-page basis.
+    def fullTitle(pageTitle='')
+        baseTitle = 'Ruby on Rails Tutorial Sample App'
+        if pageTitle.empty?
+            baseTitle
+        else
+            pageTitle + " | " + baseTitle 
+        end
+    end
 end
