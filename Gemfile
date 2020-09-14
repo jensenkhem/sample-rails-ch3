205c8922233cb7d96104978413517471d530ae43
Jensen <jensenkhem@gmail.com>
2020-09-13 19:07:43 -0600

    Make a basic user model with passowrd hashing sd
    as
    git branch

--- Gemfile
+++ Gemfile
@@ -11,6 +11,8 @@ gem 'puma', '~> 4.1'
 # Use SCSS for stylesheets
 gem 'sass-rails', '>= 6'
 gem 'bootstrap-sass', '~> 3.4', '>= 3.4.1'
+# Use bcrypt for password functionality
+gem 'bcrypt', '~> 3.1', '>= 3.1.11'
 # Transpile app-like JavaScript. Read more: https://github.com/rails/webpacker
 gem 'webpacker', ' 4.0'
 # Turbolinks makes navigating your web application faster. Read more: https://github.com/turbolinks/turbolinks
--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -5,7 +5,8 @@ class UserTest < ActiveSupport::TestCase
   #   assert true
   # end
   def setup()
-    @user = User.new(name: "Example user", email: "example@email.com")
+    @user = User.new(name: "Example user", email: "example@email.com",
+     password: "foobar", password_confirmation: "foobar")
   end
 
   test "should be valid" do
@@ -48,4 +49,30 @@ class UserTest < ActiveSupport::TestCase
       assert_not @user.valid?, "#{invalid_address.inspect} should be invalid"
     end
   end  
+
+  test "email addresses should be unique" do
+    duplicate_user = @user.dup 
+    @user.save
+    assert_not duplicate_user.valid?
+  end    
+
+  test "email addresses should be saved as lower-case" do 
+    mixed_case_email = "Foo@ExAMPle.CoM"
+    @user.email = mixed_case_email
+    @user.save
+    assert_equal mixed_case_email.downcase, @user.reload.email 
+  end
+
+  test "password should be present" do
+    @user.password = @user.password_confirmation = " " * 6
+    assert_not @user.valid? 
+  end
+
+  test "password should have a minimum length" do
+    @user.password = @user.password_confirmation = "a" * 5 
+    assert_not @user.valid?
+  end 
+
 end
+  
+      
