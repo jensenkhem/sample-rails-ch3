1e4be26cb7e6abab65fb50cb851d5e35ee20ebdf
Jensen <jensenkhem@gmail.com>
2020-09-20 20:21:11 -0600

    Added error handling for signup

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,26 @@
 class UsersController < ApplicationController
   def new
+    # Creates a new User to pass into form-for
+    @user = User.new
   end
   def show
+    # Shows a single user! (/users/[id])
     @user = User.find(params[:id])
   end
+
+  # POST request to /users
+
+  def create
+    @user = User.new(user_params) 
+    if @user.save
+      # Handle a successful save.
+    else
+      render 'new'
+    end
+  end
+
+  private
+    def user_params
+      params.require(:user).permit(:name, :email, :password, :password_confirmation)
+    end
 end
---
+++ test/integration/users_signup_test.rb
@@ -0,0 +1,20 @@
+require 'test_helper'
+
+class UsersSignupTest < ActionDispatch::IntegrationTest
+  test "invalid signup does not go through" do
+    get signup_path
+    assert_no_difference 'User.count' do
+      post signup_path, params: { 
+        user: { 
+          name: "",
+          email: "user@invalid",
+          password: "foobar",
+          password_confirmation: "barfoo"
+        }
+      }
+    end
+    assert_template 'users/new' 
+    assert_select "div#error_explanation"
+    assert_select "div.field_with_errors"
+  end
+end
